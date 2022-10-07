7bf2c460d0239e971da04c0fd6dc2f910dc2274d
Adam Seewald <adam@seewald.email>
2022-10-06 20:49:00 -0400

    implementing pcd functionality (read pcd from isdf data)

--- include/pcd_wrapper.hpp
+++ include/pcd_wrapper.hpp
@@ -5,9 +5,15 @@
 #define YTCG_PCD_WRAPPER_HPP
 
 #define NODE_PCD_WRAPPER "pointcloud_depth_wrapper"
+#define PCD_FOLDER       "/home/user"
 #define PCD_FREQ         1
 
 namespace ytcg {
+
+    struct PointCloud {
+        float x, y, z;
+	PointCloud(float _x, float _y, float _z) : x(_x), y(_y), z(_z) { }
+    };
 
     class PCDWrapper {
 
--- src/pcd_wrapper.cpp
+++ src/pcd_wrapper.cpp
@@ -1,9 +1,23 @@
 
 #include "../include/pcd_wrapper.hpp"
 
+#include <boost/algorithm/string.hpp>
+#include <boost/range/adaptors.hpp>
+#include <boost/filesystem.hpp>
+#include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
 
+#define BOOST_RANGE_ENABLE_CONCEPT_ASSERT 0
+
+using namespace boost::filesystem;
+using namespace boost::adaptors;
 using namespace ytcg;
+
+using std::string;
+using std::vector;
+using std::time_t;
 
 PCDWrapper::PCDWrapper(void) {
     timer_ = handler_.createTimer(ros::Duration(PCD_FREQ), timer_callback);
@@ -14,13 +28,51 @@ PCDWrapper::~PCDWrapper(void) { }
 
 void PCDWrapper::timer_callback(const ros::TimerEvent& _event) {
 	
-    static size_t _count = 0;
-    
+    static std::vector<PointCloud> pcd;
+    static size_t                  _count   = 0;
+    static size_t                  _hash    = 0;
+    size_t                         __hash__ = 0;
+    int                            i;
+    std::string                    line, raw_data;
+
     if (_count++ == 0) {
         ROS_INFO("callback from timer");
     }
+
+    const std::string target_path(PCD_FOLDER);
+    vector<string>    entries, __raw_data__;
     
-    // todo   
+    for (auto &entry : boost::make_iterator_range(directory_iterator(target_path), {})
+         | filtered(static_cast<bool (*)(const path &)>(&is_regular_file))
+         | filtered([&](const path &_path){
+                        return _path.filename().string().rfind("isdf_pcd_", 0) == 0; 
+                      })
+        ) {
+	entries.push_back(entry.path().filename().string());
+    }
+
+    sort(entries.begin(), entries.end());
+
+    if ((__hash__ = std::hash<std::string>{}(entries.back())) != _hash) {
+        ROS_INFO_STREAM("detected new pcd " << entries.back() << ", " << __hash__);
+        std::ifstream pcd_file(std::string(PCD_FOLDER) + "/" + entries.back());
+	std::getline(pcd_file, raw_data); // all the data are just in the first line
+	pcd_file.close();
+
+	// ROS_INFO_STREAM("debug 1 " << raw_data);
+
+	boost::split(__raw_data__, raw_data, boost::is_any_of(","));
+
+	pcd.clear();
+	for (i = 0; i+2 < __raw_data__.size(); i += 3) {
+            pcd.push_back(PointCloud(std::atof(__raw_data__.at(i).c_str()),
+                                     std::atof(__raw_data__.at(i+1).c_str()),
+                                     std::atof(__raw_data__.at(i+2).c_str())));
+	    ROS_INFO_STREAM("pcd data " << i/3 << " is " << pcd.back().z << ", " << pcd.back().y << ", " << pcd.back().z);
+	}
+
+	_hash = __hash__;
+    }
 }
 
 int main(int argc, char ** argv) {
